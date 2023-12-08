2bac1418c4398b0d6d2df4c94ae55be900e00c5a
Veronica Bravo <verobravo112012@gmail.com>
2023-12-07 22:37:52 -0300

    test haedo

---
+++ src/componentes/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="contenedor-footer">
+      <p>Todos los derechos resevados. © 2023 Harry Potter.  Diseñado por:
+        <a href='https://www.linkedin.com/in/emmanuelhaedo?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BhC9q25WdQe26VnNRbtpBQA%3D%3D'>Emmanuel Haedo. </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
